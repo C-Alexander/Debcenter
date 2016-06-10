36c37f3ba9971212bfe3e91555b35d5bb2075253
Alexander <alexander@contritio.com>
2016-06-10 23:59:21 +0200

    Add project files.

---
+++ Debcenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Debcenter.Startup))]
+namespace Debcenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
